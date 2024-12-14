a175d126b158d0410cebd323c3506fe97e1411d4
Damon <damon.bingham@hotmail.com>
2024-12-14 18:33:29 +1000

    JS
    Defined functions

---
+++ index.js
@@ -0,0 +1,32 @@
+const displayInput = document.getElementById('inputValue')
+
+// Variables
+const operators = ['-', '+', '%', '*', '/']
+
+//Functions & Operations
+
+// Handle Interactions
+function handleInteraction(value) {
+    console.log(value)
+    if operators 
+}
+
+function handleNumericInput() {
+
+}
+
+function handleOperatorInput() {
+
+}
+
+function handleEvaluate() {
+
+}
+
+function handleReset() {
+
+}
+
+function updateUi() {
+
+}
