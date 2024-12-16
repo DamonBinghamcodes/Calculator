66b6f31565008214a71829359327c56f3e2aa3b7
Damon <damon.bingham@hotmail.com>
2024-12-16 19:50:26 +1000

    HTML Issue
    updated ID in H1 element to InnerText

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const displayInput = document.getElementById('inputValue')
 
-// Variables
-const operators = ['-', '+', '%', '*', '/', '(', ')']
-let operations =[]
+//Variables
+const operators = ['-', '+', '%', '*', '/']
+let operations = []
 let currValue = ''
 
 //Functions & Operations
-
-// Handle Interactions
 function handleInteraction(value) {
-    console.log(value)
     if (operators.includes(value)) {
         console.log('Clicked an operator: ', value)
         handleOperatorInput(value)
@@ -17,26 +14,57 @@ function handleInteraction(value) {
         console.log('Clicked a numeric value: ', value)
         handleNumericInput(value)
     }
-    updateUi()
+    updateUI()
 }
 
 function handleNumericInput(value) {
+    if (value === '.' && currValue.includes('.')) { return }
     currValue += value
-    console.log('NEW VALUE: ', currValue)
 }
 
 function handleOperatorInput(value) {
-    
+    if (!currValue) {
+        return
+    }
+    if (value === '%') {
+        
+    }
+    operations.push(currValue)
+    operations.push(value)
+    currValue = ''
 }
 
 function handleEvaluate() {
-
+    if (operations.length === 0) { return }
+    let finalAmount = operations[0]
+    let prevOperator = null
+    if (!currValue) {
+        operations.pop()
+    } else {
+        operations.push(currValue)
+        currValue = ''
+    }
+    for (let i = 1; i < operations.length; i++) {
+        if (i % 2 === 0) {
+            //Numeric value
+            finalAmount = eval(`${finalAmount} ${prevOperator} ${operations[i]}`)
+        } else {
+            //Operator value
+            prevOperator = operations[i]
+        }
+    }
+    operations = []
+    currValue = finalAmount.toFixed(2)
+    updateUI()
 }
 
 function handleReset() {
-
+    currValue = ''
+    operations = []
+    updateUI()
 }
 
-function updateUi() {
-
+function updateUI() {
+    const displayString = operations.join(' ') + ' ' + currValue
+    displayInput.innerText = displayString.trim() ? displayString : '0'
 }
